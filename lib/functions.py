05b0d131fd68ce903267be1afee216dcd112d62a
Richard Hachar <richard.hachar@gmail.com>
2018-05-09 18:30:11 -0500

    Update functions.py

--- lib/functions.py
+++ lib/functions.py
@@ -2,6 +2,6 @@
 
 def rmsle_cv(model):
     n_folds = 5
-    kf = KFold(n_folds, shuffle=True, random_state=42).get_n_splits(train.values)
+    kf = KFold(n_folds, shuffle=True, random_state=1).get_n_splits(train.values)
     rmse= np.sqrt(-cross_val_score(model, train.values, y_train, scoring="neg_mean_squared_error", cv = kf))
     return(rmse)
